b00afa2f33e456aa2d1535b1a3d3dfe506ac5d72
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2023-10-04 13:57:52 +0700

    Update and rename singboxx.sh to singbox.sh

--- singbox.sh
+++ singbox.sh
@@ -63,7 +63,7 @@ newJson=$(echo "$json" | jq \
     --arg sni "$sni" \
     --arg pk "$pk" \
     --arg uuid "$uuid" \
-    '.inbounds[0].tls.reality.private_key = "'$pk'" | 
+    '.inbounds[0].tls.reality.private_key = ["'$pk'"] | 
      .inbounds[0].tls.server_name = "'$sni'" |
      .inbounds[0].users[0].uuid = "'$uuid'" |
      .inbounds[0].tls.reality.short_id += ["'$shortId'"]')
