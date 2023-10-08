b1eab84ab47f55d30ece50279768dec134f94b70
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2023-10-08 08:27:55 +0700

    Update phuc.sh

--- phuc.sh
+++ phuc.sh
@@ -1,1 +1,1 @@
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --version v1.8.3
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --version v1.8.4
