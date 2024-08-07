6eff8c0a654642566426313d60a12cb40e6db88d
Thảo mai tắm <109426941+Thaomtam@users.noreply.github.com>
2024-08-08 03:41:48 +0700

    Update hiddify-core.sh

--- hiddify-core.sh
+++ hiddify-core.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Set sing-box version and architecture
-export SING_BOX_VERSION=v1.8.8.h4
+# Set sing-box architecture
 export ARCH=$(case "$(uname -m)" in 
     'x86_64') echo 'amd64' ;;
     'x86' | 'i686' | 'i386') echo '386' ;;
@@ -23,7 +22,8 @@ fi
 rm -f /usr/bin/sing-box
 
 # Download and extract sing-box
-curl -L -o "$TMP_DIR/sing-box-linux-$ARCH.zip" "https://github.com/hiddify/hiddify-sing-box/releases/download/$SING_BOX_VERSION/sing-box-linux-$ARCH.zip" || { echo "Failed to download sing-box"; exit 1; }
+DOWNLOAD_URL="https://github.com/Thaomtam/hiddify-sing-box/releases/download/main/sing-box-linux-$ARCH.zip"
+curl -L -o "$TMP_DIR/sing-box-linux-$ARCH.zip" "$DOWNLOAD_URL" || { echo "Failed to download sing-box"; exit 1; }
 unzip "$TMP_DIR/sing-box-linux-$ARCH.zip" -d "$TMP_DIR" || { echo "Failed to extract sing-box"; exit 1; }
 
 # Move sing-box binary
